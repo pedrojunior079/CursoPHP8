d8c04d80a72474eba31ca58a428dc7f3bfa53946
Pedro Arns Junior <pedroarnsjr@outlook.com>
2023-11-22 23:52:16 -0300

    PHP8 - Variaveis

---
+++ variaveis.php
@@ -0,0 +1,39 @@
+<?php
+// VARIÁVEIS
+// =========================================================
+
+# São usadas para guardar dados:números, textos, etc.
+# podemos apresentar e modificar os seus valores ao longo do código
+# definem-se com um valor dollar ($) e um identificador (nome da variavel)
+//$variavel;
+
+#podemos definir e inciar uma variável
+// $variavel = 100;
+
+# podemos alterar o tipo de valores de uma variavel
+// $variavel = "teste;
+
+# as variaveis são Case Sensitive
+// $variavel = "uma variavel";
+// $Variavel = "outra variavel";
+
+//$1var = 0;
+//$!var = 0;
+//$variavel(1) = 0;
+//$variavel um = 0;
+//$variavel-um = 0;
+
+$var1 = 0;
+$var_um = 0;
+$varUm = 0;
+$_um = 0;
+$_1 = 0;
+
+# convenções
+$umaVariavel = 0; # camel case
+$uma_variavel = 0; # com udersores (snake case)
+$UmaVariavel = 0; # studly case (não recomendado)
+
+# DEVE SER SEMPRE MANTIDO O MESMO ESTILO AO LONGO DO PROJETO
+$variavel = 100;
+echo $variavel;
